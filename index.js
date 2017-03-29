7d18f6a07b636f437ced516ac35df40755231771
Juho Heinisuo <juho.heinisuo@reaktor.com>
2017-03-29 13:16:08 -0400

    Add lift for React

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import {Stream} from './src/create-stream'
 import * as S   from './src/stream'
+import {lift}   from './src/lift'
 
 export {
   Stream,
-  S
+  S,
+  lift
 }
 
---
+++ src/lift.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+class Observable extends React.Component {
+
+  constructor (props) {
+    super(props)
+    this.state = {value: null}
+  }
+
+  componentWillMount () {
+    this.unsubscribe = this.props.stream(value => this.setState({value}))
+  }
+
+  componentWillUnMount () {
+    this.unsubscribe()
+  }
+
+  render () {
+    const value = this.state.value
+
+    return React.isValidElement(value) ? value : <span>{value}</span>
+  }
+}
+
+export function lift (stream) {
+  return <Observable stream={stream}/>
+}
