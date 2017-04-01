0450d6a9314ac931d247fee5f96b68b7bb1d5dfe
Juho Heinisuo <juho.heinisuo@iki.fi>
2017-03-31 20:01:44 -0400

    Add more functions, reoder

--- src/stream.js
+++ src/stream.js
@@ -1,5 +1,8 @@
-export const map = (fn, stream) => sink =>
-  stream(value => sink(fn(value)))
+export const doAction = (fn, stream) => sink =>
+  stream(value => {
+    fn(value)
+    sink(value)
+  })
 
 export const filter = (fn, stream) => sink =>
   stream(value => {
@@ -7,6 +10,20 @@ export const filter = (fn, stream) => sink =>
       sink(value)
     }
   })
+
+export const flatMap = (fn, stream) => sink =>
+  stream(value => fn(value)(sink))
+
+export const fromPromise = promise => sink =>
+  promise.then(sink)
+
+export const fromEvent = (element, event) => sink => {
+  element.addEventListener(event, sink)
+  return () => element.removeEventListener(sink)
+}
+
+export const map = (fn, stream) => sink =>
+  stream(value => sink(fn(value)))
 
 export const scan = (fn, initial, stream) => sink => {
   let payload = initial
@@ -17,13 +34,18 @@ export const scan = (fn, initial, stream) => sink => {
   })
 }
 
+export const skipDuplicates = (fn, stream) => sink => {
+  let previous = null
+  return stream(value => {
+    if (!fn(previous, value)) {
+      sink(value)
+    }
+    previous = value
+  })
+}
+
 export const startWith = (initial, stream) => sink => {
   sink(initial)
   return stream(sink)
 }
 
-export const flatMap = (fn, stream) => sink =>
-  stream(value => fn(value)(sink))
-
-export const fromPromise = promise => sink =>
-  promise.then(sink)
